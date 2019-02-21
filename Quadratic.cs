4f2c57c7fb23407f5c22d060e7a9b0cdc3667eef
Pingdwende Philippe Kabore <kaborephil@gmail.com>
2019-02-21 18:29:42 -0500

    Add Qradratic.cs class

---
+++ Quadratic.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathFuctionLibrary
+{
+    class Quadratic
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+
+        public double QuadraticX1(int a, int b, int c)
+        {
+            A = a;
+            B = b;
+            C = c;
+
+            double x1;
+
+
+            if ((a < 0) && (b < 0) && (c < 0))
+            {
+                Console.WriteLine(" Please enter a positive number");
+            }
+
+            x1 = ((-b) + (Math.Sqrt((b * b) - (4 * a * c)))) / (2 * a);
+
+            return x1;
+
+        }
+
+        public double QuadraticX2(int a, int b, int c)
+        {
+            A = a;
+            B = b;
+            C = c;
+
+            double x2;
+
+
+            if ((a < 0) && (b < 0) && (c < 0))
+            {
+                Console.WriteLine(" Please enter a positive number");
+            }
+
+            x2 = ((-b) - (Math.Sqrt((b * b) - (4 * a * c)))) / (2 * a);
+
+            return x2;
+
+        }
+    }
+}
