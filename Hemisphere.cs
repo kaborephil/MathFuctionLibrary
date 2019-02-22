fa43385df89a928acbf986c9ac1ab861bea1eb09
Pingdwende Philippe Kabore <kaborephil@gmail.com>
2019-02-21 19:33:00 -0500

    Add Hemisphere.cs class

---
+++ Hemisphere.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathFuctionLibrary
+{
+    class Hemisphere
+    {
+        public double Radius { get; set; }
+
+        public double HemisphereVolume(double radius)
+        {
+            Radius = radius;
+            double Volume;
+
+            if (radius < 0)
+            {
+                Console.WriteLine(" Please enter a positive number");
+            }
+
+            Volume = ((4.0 / 3) * Math.PI * Math.Sqrt(radius) * radius) / 2;
+
+            return Volume;
+        }
+    }
+}
